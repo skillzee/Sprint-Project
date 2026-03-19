dfa28df845b7a83b563a59375b8acf6aa628f5b0
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2026-03-19 12:05:55 +0530

    Starting Hotel Service with Models and DTOs of hotel and rooms

---
+++ Services/TravelApp.Services.Hotel/DTOs/HotelDtos.cs
@@ -0,0 +1,41 @@
+﻿namespace TravelApp.Services.Hotel.DTOs
+{
+    public record HotelDto(
+        int Id,
+        int Name,
+        string City,
+        string Address,
+        string Description,
+        double Rating,
+        string ImageUrl
+    );
+
+    public record RoomDto(
+        int Id,
+        int HotelId,
+        string HotelName,
+        string RoomType,
+        decimal PricePerNight,
+        int Capacity,
+        bool IsAvailable
+    );
+
+
+    public record CreateHotelDto(
+        string Name,
+        string City,
+        string Address,
+        string Description,
+        double Rating,
+        string ImageUrl
+    );
+
+
+    public record CreateRoomDto(
+        int HotelId,
+        string RoomType,
+        decimal PricePerNight,
+        int Capacity,
+        bool IsAvailable
+    );
+}
---
+++ Services/TravelApp.Services.Hotel/Models/Hotel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TravelApp.Services.Hotel.Models
+{
+    public class Hotel
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string City { get; set; } = "";
+        public string Address { get; set; } = "";
+        public string Description { get; set; } = "";
+        public double Rating { get; set; }
+        public string ImageUrl { get; set; } = "";
+        public ICollection<Room> Rooms = new List<Room>();
+
+
+
+    }
+
+
+
+    public class Room
+    {
+        public int Id { get; set; }
+        public int HotelId { get; set; }
+        [ForeignKey("HotelId")]
+        public Hotel Hotel { get; set; } = null!;
+        public string RoomType { get; set; } = "";
+        public decimal PricePerNight {  get; set; }
+        public int capacity { get; set; }
+        public bool IsAvailable { get; set; } = true;
+
+    }
+}
