1e8274294a0dca7320252f7cc60cdca849eb39a7
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2026-03-22 21:22:48 +0530

    Initialising frontend with angular 19 with pages and services

---
+++ Web/travel-app-web/src/app/pages/flights/flights.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flights',
+  imports: [],
+  templateUrl: './flights.component.html',
+  styleUrl: './flights.component.css'
+})
+export class FlightsComponent {
+
+}
