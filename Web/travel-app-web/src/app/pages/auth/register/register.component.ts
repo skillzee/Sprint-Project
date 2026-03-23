10d2e0f30aff0ac0b2e25bef200144845bccd3cb
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2026-03-23 10:54:53 +0530

    register component done

--- Web/travel-app-web/src/app/pages/auth/register/register.component.ts
+++ Web/travel-app-web/src/app/pages/auth/register/register.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  name = ''; email = ''; password = '';
+  loading = signal(false);
+  error = signal(''); 
+  success = signal('');
+
+  constructor(private auth: AuthService, private router: Router) { }
+
+  register() {
+    this.loading.set(true);
+    this.error.set('');
+    this.auth.register(this.name, this.email, this.password).subscribe({
+      next: () =>{
+        this.success.set('Account created!');
+        this.router.navigate(['/'])
+      },
+      error: err => { this.error.set(err.error?.message || 'Registration failed'); this.loading.set(false); }
+    });
+  }
 }
