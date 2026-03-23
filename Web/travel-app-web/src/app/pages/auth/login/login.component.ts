fdf82f4cae26ae505550cbfd4dd6f0cc938345af
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2026-03-23 19:32:58 +0530

    login component completed

--- Web/travel-app-web/src/app/pages/auth/login/login.component.ts
+++ Web/travel-app-web/src/app/pages/auth/login/login.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email = ''; password = '';
+  loading = signal(false); error = signal('');
+
+   constructor(private auth: AuthService, private router: Router) {}
+
+    login() {
+    this.loading.set(true); this.error.set('');
+    this.auth.login(this.email, this.password).subscribe({
+      next: () => this.router.navigate(['/']),
+      error: err => { this.error.set(err.error?.message || 'Invalid credentials'); this.loading.set(false); }
+    });
+  }
 
 }
