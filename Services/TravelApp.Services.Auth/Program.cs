0b671bf3622bc9be84cb7a69fd1acd34fd7ac1fd
skillzee <parthsharmaisnumber1@gmail.com>
2026-04-07 14:38:59 +0530

    adding auto migrate in db

--- Services/TravelApp.Services.Auth/Program.cs
+++ Services/TravelApp.Services.Auth/Program.cs
@@ -54,4 +54,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AuthDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
